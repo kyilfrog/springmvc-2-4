afe24cbdd13eff535b56bfa4f182ee8230e86a74
kyilfrog <160857010+kyilfrog@users.noreply.github.com>
2024-04-16 20:44:12 +0900

    Validator 분리1

--- src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -26,6 +26,7 @@ import java.util.Map;
 public class ValidationItemControllerV2 {
 
     private final ItemRepository itemRepository;
+    private final ItemValidator itemValidator;
 
     @GetMapping
     public String items(Model model) {
@@ -168,10 +169,10 @@ public class ValidationItemControllerV2 {
         return "redirect:/validation/v2/items/{itemId}";
     }
     
-    @PostMapping("/add")
+    //@PostMapping("/add")
     public String addItemV4(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
         
-    	// 주석을 해제하면 
+    	// 주석을 해제하면 (가격은 1,000 ~ 1,000,000 까지 허용합니다.) 에러코드가 사라진다. 
     	/*
     	if(bindingResult.hasErrors()) {
     		return "validation/v2/addForm";
@@ -206,6 +207,26 @@ public class ValidationItemControllerV2 {
     			bindingResult.reject("totalPriceMin", new Object[] {10000, resultPrice}, null);
     		}
     	}
+    	
+    	//검증에 실패하면 다시 입력 폼으로 보낸다
+    	if (bindingResult.hasErrors()) {
+    		log.info("errors = {}", bindingResult);
+    		
+    		return "validation/v2/addForm";
+    	}
+    	
+    	//성공 로직
+    	Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+    
+    @PostMapping("/add")
+    public String addItemV5(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+        
+    	//ItemValidator 클래스 가져와서 검증
+    	itemValidator.validate(item, bindingResult);
     	
     	//검증에 실패하면 다시 입력 폼으로 보낸다
     	if (bindingResult.hasErrors()) {
