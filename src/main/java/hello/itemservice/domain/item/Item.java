e8e9e8e446852290e3942eed120df0fb83d71dd2
kyilfrog <160857010+kyilfrog@users.noreply.github.com>
2024-04-17 22:10:39 +0900

    Bean Validation 스프링 적용

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,13 +1,26 @@
 package hello.itemservice.domain.item;
 
+import org.hibernate.validator.constraints.Range;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class Item {
 
     private Long id;
+    
+    @NotBlank(message = "공백은 안되여~~")
     private String itemName;
+    
+    @NotNull
+    @Range(min = 1000, max = 1000000)
     private Integer price;
+    
+    @NotNull
+    @Max(9999)
     private Integer quantity;
 
     public Item() {
